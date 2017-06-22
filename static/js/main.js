c55439ba09bbb71502d4d65974920d353f324b49
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-06-22 14:41:46 -0700

    main.js: added event handler to populate map with markers based on entered category

--- static/js/main.js
+++ static/js/main.js
@@ -14,4 +14,34 @@ function initMap() {
 window.initMap = initMap
 
 
+// click event searching for categories
+$("#find").on("click", () => {
+
+    let searchTerm = $("#search").val();
+
+    // call to json route to get data
+    fetch("/getBusinessInfo.json"+"?searchTerm="+searchTerm).then( (response) => {
+
+        return response.json()
+    }).then( (value) => {
+
+         return value[0]
+
+    }).then ( (businesses) => {
+        try {
+            businesses.forEach( (business) => {
+
+              // add marker to for ever business returned
+              jsMap.addMarker(business)
+
+            });
+        }
+
+        // handing error when no results were returned - todo: handle error
+        catch(TypeError) {
+            console.log(TypeError);
+        }
+    })
+
+})
 
