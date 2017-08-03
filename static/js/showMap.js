70ecfd89b29c7d4c1a1c3ec0917ef6066d88e496
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-08-03 15:19:35 -0700

    showMap.js: - made initmap generic to take different divs
    - get lat/lng from database instead of Google Maps API call

--- static/js/showMap.js
+++ static/js/showMap.js
@@ -6,11 +6,10 @@ class showMap {
     constructor() {
       this.gMarkers = [];
       this.map;
-      this.initMap();
-
+      this.initMap("map");
     }
-    initMap() {
-        this.map = new google.maps.Map(document.getElementById("map"), {
+    initMap(div) {
+        this.map = new google.maps.Map(document.getElementById(div), {
           // center map on SF
           center: {lat: 37.7749, lng: -122.4194},
           zoom: 12
@@ -18,40 +17,29 @@ class showMap {
     }
     addMarker(business) {
 
-        // making API call to geocode address into lat/lng to create markers
-        // placeholder for now -- will add lat/lng to db & make api call when
-        // populating the database
-        fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+
-              business.address+"&key="+gMapsKey).then(
-                (response) => {
-                    return response.json() }).then( (results) => {
+          var newMarker = {lat: business.lat, lng: business.lng}
 
-                      // getting lat/lng of address passed to API
-                      var newMarker = results.results[0].geometry.location
+          // create and set a marker and specific location
+          var marker = new google.maps.Marker({
+              map: this.map,
+              position: newMarker,
+              title: business.name
+            });
 
-                      // create and set a marker and specific location
-                      var marker = new google.maps.Marker({
-                          map: this.map,
-                          position: newMarker,
-                          title: business.name
-                        });
+          this.gMarkers.push(marker);
 
-                      this.gMarkers.push(marker);
+          var contentString = "<h4><a href='/businesses/"
+                              +business.business_id+"'>"
+                              +business.name+"</a></h4>"
+                              +"<p>"+business.address+"</p>";
 
-                      var contentString = "<h4><a href='/businesses/"
-                                          +business.business_id+"'>"
-                                          +business.name+"</a></h4>"
-                                          +"<p>"+business.address+"</p>";
+          var infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
 
-                      var infowindow = new google.maps.InfoWindow({
-                        content: contentString
-                      });
-
-                      marker.addListener('click', function() {
-                        infowindow.open(map, marker);
-                      });
-
-                    })
+          marker.addListener('click', function() {
+            infowindow.open(map, marker);
+          });
 
       }
       deleteMarkers() {
