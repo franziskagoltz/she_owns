26f8cc305bdda407de24b82f78260142248e73b2
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-08-03 16:01:20 -0700

    single-map.js: create class to display map centered on business location in new template

---
+++ static/js/single-map.js
@@ -0,0 +1,28 @@
+// single map for business details page
+
+class SingleMap {
+
+    constructor() {
+        this.singleMap;
+        this.initSingleMap();
+    }
+
+    initSingleMap() {
+        this.singleMap = new google.maps.Map(document.getElementById("single_map"), {
+          // center map on business location
+          center: latlng,
+          zoom: 12
+        });
+    }
+
+    addMarker() {
+      // create and set a marker and specific location
+      var marker = new google.maps.Marker({
+          map: this.singleMap,
+          position: latlng,
+        });
+    }
+
+}
+
+export default SingleMap;
